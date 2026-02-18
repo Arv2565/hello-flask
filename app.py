b5ccdd320811635cb35cb8fda750f13aeb4d3f46
Arv2565 <arvindsunil7@gmail.com>
2026-02-18 05:12:32 +0000

    testing workflow

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello-bro"
+    return "hello-nigga"
 
 @app.route("/healthz")
 def health():
-    return "OK", 200
+    return "healthz is working", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
